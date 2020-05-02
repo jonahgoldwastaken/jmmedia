6265209196cc252875d7499ff136350711bcc649
Jonah Meijers <jonahmeijers97@gmail.com>
2020-05-02 11:58:16 +0200

    added center alignment option for h1

--- components/Text/Headings/HeadingOne.ts
+++ components/Text/Headings/HeadingOne.ts
@@ -1,12 +1,18 @@
 import styled from 'styled-components'
 import { BaseHeading, HeadingProps } from './BaseHeading'
 
-export const HeadingOne = styled.h1<HeadingProps>`
+type HeadingOneProps = {
+  centre?: boolean
+}
+
+export const HeadingOne = styled.h1<HeadingProps & HeadingOneProps>`
   ${BaseHeading};
+  margin: ${props => props.theme.spacing[1]} 0;
   font-size: ${props => props.theme.fontSizes[4]};
   font-weight: ${props => props.theme.fontWeights[3]};
   color: ${props =>
     props.colour
       ? props.theme.colours[props.colour]
       : props.theme.colours.secondary};
+  ${props => props.centre && 'text-align: center'};
 `
