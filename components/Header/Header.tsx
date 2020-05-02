53cf845a791e12c6634eff10b71d441d7a4da9a8
Jonah Meijers <jonahmeijers97@gmail.com>
2020-05-02 12:50:47 +0200

    header spacing

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,8 +2,7 @@ import styled from 'styled-components'
 
 export const Header = styled.header`
   background: ${props => props.theme.colours.tertiary};
-  padding: ${props => props.theme.spacing[1]};
-  position: relative;
+  padding: 0 0 ${props => props.theme.spacing[1]};
   display: flex;
   justify-content: space-between;
   align-items: center;
