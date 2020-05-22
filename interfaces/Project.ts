44593a72ebda4d3ffa7f11702d09d8d5c743c8f6
Jonah Meijers <jonahmeijers97@gmail.com>
2020-05-22 21:38:57 +0200

    added content thingy

--- interfaces/Project.ts
+++ interfaces/Project.ts
@@ -1,6 +1,22 @@
+type headingValue = string
+type paragraphValue = string
+type imageValue = {
+  alt: string
+  srcSet: string[]
+}
+type rowValue = Array<imageValue>
+type filmValue = string
+
+type contentValue =
+  | headingValue
+  | paragraphValue
+  | imageValue
+  | rowValue
+  | filmValue
+
 export interface ProjectContent {
   type: 'heading' | 'paragraph' | 'image' | 'row' | 'film'
-  content: string
+  content: contentValue
   alt?: string
   size?: number
 }
