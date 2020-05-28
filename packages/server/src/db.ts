6e96a6f3c8759432fea0bc0cc3966ae9c84da3f3
Jonah Meijers <jonahmeijers97@gmail.com>
2020-05-28 15:58:50 +0200

    remove console logs

--- packages/server/src/db.ts
+++ packages/server/src/db.ts
@@ -3,7 +3,6 @@ const { MONGO_URL, MONGO_DATABASE } = process.env
 
 export const connectToDB = async () => {
   if (MONGO_URL && MONGO_DATABASE) {
-    console.log(MONGO_URL, MONGO_DATABASE)
     try {
       await mongoose.connect(MONGO_URL, {
         useNewUrlParser: true,
@@ -13,7 +12,7 @@ export const connectToDB = async () => {
       })
       return true
     } catch (err) {
-      console.log(err)
+      console.error(err)
       return false
     }
   } else {
